8f772956f4964562f9f756333d9880a8831f720b
luiggy macias <ferrinluiggy@gmail.com>
2023-04-10 14:00:30 -0500

    se agrega join

--- backend/src/controllers/properties.controller.ts
+++ backend/src/controllers/properties.controller.ts
@@ -1,15 +1,13 @@
 import { Request, Response, Router } from "express";
-import Property from "../models/Property";
-import Status from "../models/Status";
+// import Status from "../models/Status";
 import { sequelize } from "../db";
 import { Op } from "sequelize";
+import { property, status, status_history } from "../models/init-models";
 
 const get = async (req: Request, res: Response) => {
   try {
     let { address, city, year } = req.query;
     const _year = parseInt(year as string);
-    const status = await Status.findAll();
-    const status_id = status.map((item) => item.id);
     const filtros: {
       status_id?: {
         [Op.in]: number[];
@@ -22,11 +20,6 @@ const get = async (req: Request, res: Response) => {
       };
       year?: number;
     } = {};
-    if (status_id.length > 0) {
-      filtros.status_id = {
-        [Op.in]: status_id,
-      };
-    }
     if (address) {
       filtros.address = {
         [Op.iLike]: `%${address}%`,
@@ -41,15 +34,21 @@ const get = async (req: Request, res: Response) => {
       filtros.year = _year;
     }
 
-    const property = await Property.findAll({
+    const p = await property.findAll({
+      attributes: ["address", "city", "price", "description"],
       where: {
         ...filtros,
       },
+      include: [
+        {
+          model: status_history,
+        },
+      ],
     });
     return res.status(200).json({
       message: "Se obtuvieron las propiedades",
       // data: { property, status, status_id },
-      data: property,
+      data: p,
     });
   } catch (error: any) {
     console.log(error);
---
+++ backend/src/models/init-models.ts
@@ -0,0 +1,10 @@
+import property from "./property";
+import status from "./status";
+import status_history from "./status_history";
+
+property.hasMany(status_history, { foreignKey: "property_id" });
+status_history.belongsTo(property, { foreignKey: "property_id" });
+status_history.belongsTo(status, { foreignKey: "status_id" });
+status.hasMany(status_history, { foreignKey: "status_id" });
+
+export { property, status, status_history };
